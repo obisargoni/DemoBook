85b45d45d0e838027ad87b1cf60af6a824402ed6
Obi Sargoni <othompsarg@hotmail.com>
2022-12-07 15:06:11 +0000

    Getting link to colab working

---
+++ _build/jupyter_execute/chapters/chapter1/introduction.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction
+# 
+# *Obi Thompson Sargoni*
+# 
+# 
+
+# In[1]:
+
+
+print("This is a text book made from notebooks")
+
+
+# In[2]:
+
+
+import numpy as np
+np.zeros((10,10,10))
+
+
+# In[ ]:
+
+
+
+
